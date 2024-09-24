7074ce4f0fa3a68e0aea36102791520ce7865fe5
won@aimmo.co.kr <won@aimmo.co.kr>
2024-09-24 10:42:15 +0900

    [main] fix : run_mode fix : fix logger in main : fix log_trace.py : fix backend_api.py

--- lib/args.py
+++ lib/args.py
@@ -5,8 +5,10 @@ import argparse
 def get_args():
 
     parser = argparse.ArgumentParser(description='Mongodb Log Monitoring.')
-    parser.add_argument('--filepath', dest='filepath', action='store', default="/data/log/mongodb.log")
-    parser.add_argument('--run-mode', dest='run_mode', action='store', default="publisher")
+    parser.add_argument('--filepath', dest='filepath',
+                        action='store', default="/data/log/mongod.log")
+    parser.add_argument('--run-mode', dest='run_mode',
+                        action='store', default="publisher")
     args = parser.parse_args()
     
     mongodb_log_path = args.filepath
@@ -14,17 +16,17 @@ def get_args():
 
     try:
         mongodb_log_path_env = os.environ['MONGO_LOG']
-    except Exception as e:
+    except Exception:
         mongodb_log_path_env = None
-    
+        
     try:
         run_mode_env = os.environ['RUN_MODE']
-    except Exception as e:
+    except Exception:
         run_mode_env = None
     
     if run_mode_env is not None:
         run_mode = run_mode_env
-    
+        
     if mongodb_log_path_env is not None:
         mongodb_log_path = mongodb_log_path_env
         
