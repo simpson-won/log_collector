540aa2dbe463a3a40cfc911c72aaa7231c712da8
won@aimmo.co.kr <won@aimmo.co.kr>
2024-11-13 18:08:27 +0900

    [dev] - mysql의 read/write 분리 적용 - coding convention 적용

--- service/user_cmds_svc.py
+++ service/user_cmds_svc.py
@@ -1,69 +1,76 @@
+"""service library for user_cmds"""
 from model.user_command import UserCommand, table_name
-from service import db_handle
+from service import db_write_handle, db_read_handle
 from lib.mysql import select_datas, insert_data
 from service.user_access_svc import select_user_by_ctx_db_client, select_user_client_by_ctx_db_dbs
 import logging
 
 
 def select_value(cursor=None, value: UserCommand = None):
+    """select value from user_cmds"""
     value_list = []
-    if cursor is None:
-        t_cursor = db_handle.cursor()
-    else:
-        t_cursor = cursor
-    values = select_datas(db_handle,
-                          cursor=t_cursor,
+    values = select_datas(db_read_handle,
                           table=table_name,
                           where=value.where_all())
     for val in values:
         value_list.append(val)
-    if cursor is None:
-        t_cursor.close()
     return value_list
 
 
-def insert_value(values: list, cursor=None, auto_commit=True, filter_str="", database_name="", host="", real_write=True):
+def insert_value(values: list, cursor=None,
+                 auto_commit=True, filter_str="",
+                 database_name="", host="",
+                 real_write=True):
+    """insert single value to user_cmds"""
     if len(values) != 6:
         return
     if cursor is None:
-        t_cursor = db_handle.cursor()
+        t_cursor = db_write_handle.cursor()
     else:
         t_cursor = cursor
-    user = select_user_by_ctx_db_client(cursor=t_cursor,
+    user = select_user_by_ctx_db_client(handle=db_read_handle,
                                         client=values[0],
-                                        db=values[4],
-                                        ctx=values[2],
-                                        database_name=database_name,
-                                        host=host)
+                                        ctx=values[2])
+    if user == "Unknown":
+        user = select_user_by_ctx_db_client(handle=db_write_handle,
+                                            client=values[0],
+                                            ctx=values[2])
     values.append(user)
     values.append(filter_str)
     values.append(database_name)
     values.append(host)
     user_cmd = UserCommand.create(*values)
     if real_write:
-        insert_data(conn=db_handle,
+        insert_data(conn=db_write_handle,
                     cursor=t_cursor,
                     table=table_name,
                     value=user_cmd,
                     auto_commit=auto_commit)
     else:
-        logging.info(f"table = {table_name} data = {user_cmd}")
+        logging.info("table = %s data = %s", table_name, user_cmd)
 
     if cursor is None:
         t_cursor.close()
 
 
-def insert_value_1(values: list, cursor=None, auto_commit=True, filter_str="", database_name="", host="", real_write=True):
+def insert_value_1(values: list, cursor=None,
+                   auto_commit=True, filter_str="",
+                   database_name="", host="",
+                   real_write=True):
+    """insert value"""
     if len(values) != 5:
         return
     if cursor is None:
-        t_cursor = db_handle.cursor()
+        t_cursor = db_write_handle.cursor()
     else:
         t_cursor = cursor
-    user, client = select_user_client_by_ctx_db_dbs(cursor=t_cursor,
-                                                    db=values[3],
+    user, client = select_user_client_by_ctx_db_dbs(handle=db_read_handle,
                                                     ctx=values[1],
                                                     database_name=database_name)
+    if user is None:
+        user, client = select_user_client_by_ctx_db_dbs(handle=db_write_handle,
+                                                        ctx=values[1],
+                                                        database_name=database_name)
     values.insert(0, client)
     values.append(user)
     values.append(filter_str)
@@ -71,13 +78,12 @@ def insert_value_1(values: list, cursor=None, auto_commit=True, filter_str="", d
     values.append(host)
     user_cmd = UserCommand.create(*values)
     if real_write:
-        insert_data(conn=db_handle,
+        insert_data(conn=db_write_handle,
                     cursor=t_cursor,
                     table=table_name,
                     value=user_cmd,
                     auto_commit=auto_commit)
     else:
-        logging.info(f"table = {table_name} data = {user_cmd}")
-
+        logging.info("table = %s data = %s", table_name, user_cmd)
     if cursor is None:
         t_cursor.close()
